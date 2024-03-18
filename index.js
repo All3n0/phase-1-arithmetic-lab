a07cb9c0d44ae7407597097f8ef2b5f8575af109
ALLAN <allankipr6@gmail.com>
2024-03-18 21:17:59 +0300

    new

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
-// Write your code here
+ let num1 = 2
+let num2 =31 
+const multiply=num1 * num2
+ let num3 = 36
+ let num4 = 8
+const mod  =  num3%num4;
+
+const numbers=[4,5,6,7,8,20];
+const max =Math.max(...numbers);
+const random = Math.floor(Math.random()*Number.MAX_SAFE_INTEGER+1);
